d04e08dcf616277e81e6414152f99748721965c8
Errico Bruno Santoriello <e.santoriello1@studenti.unimol.it>
2025-06-05 15:56:27 +0200

    Aggiunto commento esplicativo per la splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 
 // Splash Screen for BoscoPlanner
+
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
